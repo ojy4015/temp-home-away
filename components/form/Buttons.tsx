3c61b6c381d95c05282e2b7faf567ace69bf3e1f
ojy4015 <ojy401@naver.com>
2024-11-07 08:32:28 +0900

    fix error fourth

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { ReloadIcon } from '@radix-ui/react-icons';
-import { Button } from '@/components/ui/button';
 import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
 import { SignInButton } from '@clerk/nextjs';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { LuTrash2, LuPenSquare } from 'react-icons/lu';
 type btnSize = 'default' | 'lg' | 'sm';
 
 type SubmitButtonProps = {
@@ -25,7 +25,7 @@ export function SubmitButton({
     <Button
       type="submit"
       disabled={pending}
-      className={`capitalize ${className}`}
+      className={`capitalize ${className} `}
       size={size}
     >
       {pending ? (
@@ -56,11 +56,7 @@ export const CardSignInButton = () => {
   );
 };
 
-// favorite 에 따라 버튼의 아이콘을 바꾼다.
 export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
-  // 마지막 양식 제출의 status 정보를 제공하는 Hook이다.
-  //status 정보를 얻을(이 hook을 호출할) 컴포넌트가 <form> 내부에 렌더링되어야 한다
-  // returns pending
   const { pending } = useFormStatus();
   return (
     <Button
@@ -76,6 +72,34 @@ export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
       ) : (
         <FaRegHeart />
       )}
+    </Button>
+  );
+};
+
+type actionType = 'edit' | 'delete';
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case 'edit':
+        return <LuPenSquare />;
+      case 'delete':
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid action type: ${never}`);
+    }
+  };
+
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="link"
+      className="p-2 cursor-pointer"
+    >
+      {pending ? <ReloadIcon className=" animate-spin" /> : renderIcon()}
     </Button>
   );
 };
