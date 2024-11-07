66a6fa0a5faaa16be0945f6ade22f4fe218c7b88
ojy4015 <ojy401@naver.com>
2024-11-07 09:21:51 +0900

    fix error nine

--- utils/LoadingCards.tsx
+++ utils/LoadingCards.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../ui/skeleton';
 
 function LoadingCards() {
   return (
-    <div className='mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <SkeletonCard />
       <SkeletonCard />
       <SkeletonCard />
@@ -14,9 +14,9 @@ function LoadingCards() {
 export function SkeletonCard() {
   return (
     <div>
-      <Skeleton className='h-[300px] rounded-md' />
-      <Skeleton className='h-4 mt-2 w-3/4' />
-      <Skeleton className='h-4 mt-2 w-1/2' />
+      <Skeleton className="h-[300px] rounded-md" />
+      <Skeleton className="h-4 mt-2 w-3/4" />
+      <Skeleton className="h-4 mt-2 w-1/2" />
     </div>
   );
 }
